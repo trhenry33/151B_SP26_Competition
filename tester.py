ca2565cf8df3782c2e1be0d9385f312ce6447fef
Travis Henry <travman.henry1@gmail.com>
2026-05-21 00:06:59 -0700

    add baseline results

--- tester.py
+++ tester.py
@@ -9,7 +9,7 @@ print("imports work")
 
 #imports
 #baseline is 60% with first 10
-#basline is --% with first 20
+#basline is 50% with first 20 - MC: 4/9 FR: 6/11
 
 #to test: rm results/count.txt and change the output path on this file to start a new file to check.
 
